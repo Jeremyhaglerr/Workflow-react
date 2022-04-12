aee7b293842197966bbbc94889a2943758a15503
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-04-12 12:39:50 -0500

    fix error displaying title

--- src/components/Cards/IssueCard/IssueCard.jsx
+++ src/components/Cards/IssueCard/IssueCard.jsx
@@ -5,7 +5,7 @@ const IssueCard = (props) => {
   return (
     <Link to={`/issues/${props.issue.id}`} className="card">
       <div className="card-content">
-        <h2 className="card-title">{props.issue.name}</h2>
+        <h2 className="card-title">{props.issue.title}</h2>
         <p>{props.issue.error}</p>
         <p><small>{props.issue.description}</small></p>
       </div>
--- src/components/Cards/TaskCard/TaskCard.jsx
+++ src/components/Cards/TaskCard/TaskCard.jsx
@@ -5,7 +5,7 @@ const TaskCard = (props) => {
   return (
     <Link to={`/tasks/${props.task.id}`} className="card">
       <div className="card-content">
-        <h2 className="card-title">{props.task.name}</h2>
+        <h2 className="card-title">{props.task.title}</h2>
         <p><small>{props.task.description}</small></p>
       </div>
     </Link>
